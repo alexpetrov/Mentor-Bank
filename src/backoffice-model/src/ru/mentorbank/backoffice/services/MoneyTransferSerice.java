52b6031ac7c96c06dc86d045d7b5ebb06ff71c3a
Alexander Petrov <alexpetrov.rb@gmail.com>
2011-03-24 22:46:00 +0300

    Remove Deleted files

--- src/backoffice-model/src/ru/mentorbank/backoffice/services/MoneyTransferSerice.java
+++
@@ -1,9 +0,0 @@
-package ru.mentorbank.backoffice.services;
-
-import ru.mentorbank.backoffice.model.transfer.TransferRequest;
-import ru.mentorbank.backoffice.services.exceptions.TransferException;
-
-public interface MoneyTransferSerice {
-
-	public void transfer(TransferRequest request) throws TransferException;
-}
--- src/backoffice-model/src/ru/mentorbank/backoffice/services/exceptions/TransferException.java
+++
@@ -1,7 +0,0 @@
-package ru.mentorbank.backoffice.services.exceptions;
-
-public class TransferException extends Exception {
-
-	private static final long serialVersionUID = 1L;
-
-}
